7293c89e1e2e3065694745c6a515e610af6c36b9
YonasValentin <eaayokli@students.eaaa.dk>
2022-03-18 13:14:23 +0100

    [added] firestoreConfig

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
+
+// firebaseConfig object copied from project in Firebase Console
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWuZHP8CJtG3EqtHYcvnaKXsMQxxKfENE',
+  authDomain: 'dininfluencer-9730c.firebaseapp.com',
+  projectId: 'dininfluencer-9730c',
+  storageBucket: 'dininfluencer-9730c.appspot.com',
+  messagingSenderId: '888887205865',
+  appId: '1:888887205865:web:705ee6e1d18ba67d347578',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export const usersRef = collection(db, 'users'); // reference to users collection i firestore
+export const postsRef = collection(db, 'posts'); // reference to posts collection i firestore
