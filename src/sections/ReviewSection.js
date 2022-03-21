8054c57c693b66b2be99a6c0aa9053761f41d726
YonasValentin <eaayokli@students.eaaa.dk>
2022-03-21 21:37:08 +0100

    [added] swiperJS added

--- src/sections/ReviewSection.js
+++ src/sections/ReviewSection.js
@@ -1,6 +1,15 @@
+import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
 export default function ReviewSection() {
-  const reviewsArray = [
+  const reviewsList = [
     {
+      reviewID: 1,
       reviewQuote:
         'Labore ut velit sunt ad irure cupidatat ad consectetur commodo Lorem excepteur cupidatat ullamco aliqua.',
       reviewProfileImage:
@@ -11,6 +20,37 @@ export default function ReviewSection() {
       reviewPosition: 'Lector',
     },
     {
+      reviewID: 2,
+      reviewQuote: 'Nulla tempor pariatur duis cillum ad reprehenderit.',
+      reviewProfileImage:
+        'https://images.pexels.com/photos/11384153/pexels-photo-11384153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      reviewProfileImageAltTxt:
+        'Labore magna elit exercitation consequat enim in.',
+      reviewName: 'Larry Briggs',
+      reviewPosition: 'Lector',
+    },
+    {
+      reviewID: 3,
+      reviewQuote: 'Nulla tempor pariatur duis cillum ad reprehenderit.',
+      reviewProfileImage:
+        'https://images.pexels.com/photos/11384153/pexels-photo-11384153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      reviewProfileImageAltTxt:
+        'Labore magna elit exercitation consequat enim in.',
+      reviewName: 'Larry Briggs',
+      reviewPosition: 'Lector',
+    },
+    {
+      reviewID: 4,
+      reviewQuote: 'Nulla tempor pariatur duis cillum ad reprehenderit.',
+      reviewProfileImage:
+        'https://images.pexels.com/photos/11384153/pexels-photo-11384153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      reviewProfileImageAltTxt:
+        'Labore magna elit exercitation consequat enim in.',
+      reviewName: 'Larry Briggs',
+      reviewPosition: 'Lector',
+    },
+    {
+      reviewID: 5,
       reviewQuote: 'Nulla tempor pariatur duis cillum ad reprehenderit.',
       reviewProfileImage:
         'https://images.pexels.com/photos/11384153/pexels-photo-11384153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -21,79 +61,41 @@ export default function ReviewSection() {
     },
   ];
 
-  let slideIndex = 1;
-  showSlides(slideIndex);
-
-  function currentSlide(n) {
-    showSlides((slideIndex = n));
-  }
-
-  function showSlides(n) {
-    let i;
-    let slides = document.querySelector('.review-section__review-wrapper');
-    let dots = document.querySelector('.review-section__dot');
-
-    const reviewQuote = document.querySelector('.review-section__quote');
-    const reviewProfileImage = document.querySelector(
-      '.review-section__profile-image'
-    );
-    const reviewProfileImageAltTxt = document.querySelector(
-      '.review-section__profile-image'
-    );
-    const reviewName = document.querySelector('.review-section__name');
-    const reviewPosition = document.querySelector('.review-section__position');
-
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
-    }
-
-    reviewQuote.innerText = reviewsArray[i].reviewQuote;
-    reviewProfileImage.src = reviewsArray[i].reviewProfileImage;
-    reviewProfileImageAltTxt.alt = reviewsArray[i].reviewProfiltImageAltTxt;
-    reviewName.innerText = reviewsArray[i].reviewName;
-    reviewPosition.innerText = reviewsArray[i].reviewPosition;
-
-    slides[slideIndex - 1].style.display = 'block';
-    dots[slideIndex - 1].className += ' active';
-  }
-
   return (
     <main className="review-section">
       <div className="review-section__container">
         <h2 className="review-section__heading">
           What our clients have to say
         </h2>
-        <aside className="review-section__review-box">
-          <div classname="review-section__review-wrapper">
-            <p className="review-section__quote"></p>
-            <img className="review-section__profile-image"></img>
-            <h5 className="review-section__name"></h5>
-            <p className="review-section__position"></p>
-          </div>
-          <div className="review-section__dots-wrapper">
-            <span
-              className="review-section__dot"
-              onClick={currentSlide(1)}
-            ></span>
-            <span
-              className="review-section__dot"
-              onClick={currentSlide(2)}
-            ></span>
-            <span
-              className="review-section__dot"
-              onClick={currentSlide(3)}
-            ></span>
-          </div>
-        </aside>
+        <Swiper
+          modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          navigation
+          autoplay={true}
+          loop={true}
+          pagination={{ clickable: true }}
+          onReachEnd={() => {
+            /*...*/
+          }}
+        >
+          {reviewsList.map((review) => (
+            <SwiperSlide key={review.id}>
+              <div className="review-section__review-box">
+                <h2 className="review-section__quote">{review.reviewQuote}</h2>
+                <figure className="review-section__image-wrapper">
+                  <img
+                    src={review.reviewProfileImage}
+                    alt={review.reviewProfileImageAltTxt}
+                  ></img>
+                </figure>
+                <p className="review-section__name">{review.reviewName}</p>
+                <p className="review-section__position">
+                  {review.reviewPosition}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </main>
   );
