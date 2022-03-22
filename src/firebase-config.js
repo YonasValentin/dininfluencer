ecaed4ba8871d5518cddf39743fd83d8c4d13eae
YonasValentin <eaayokli@students.eaaa.dk>
2022-03-22 12:33:20 +0100

    [added] json file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,33 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection } from 'firebase/firestore';
+import { addDoc, getFirestore, collection } from 'firebase/firestore';
 
 // firebaseConfig object copied from project in Firebase Console
 const firebaseConfig = {
-  apiKey: 'AIzaSyDWuZHP8CJtG3EqtHYcvnaKXsMQxxKfENE',
-  authDomain: 'dininfluencer-9730c.firebaseapp.com',
-  projectId: 'dininfluencer-9730c',
-  storageBucket: 'dininfluencer-9730c.appspot.com',
-  messagingSenderId: '888887205865',
-  appId: '1:888887205865:web:705ee6e1d18ba67d347578',
+  apiKey: 'AIzaSyBLp1E6sOXO0UOUoGPH4-uqsmpDbrixvvE',
+  authDomain: 'dininfluencer-test.firebaseapp.com',
+  projectId: 'dininfluencer-test',
+  storageBucket: 'dininfluencer-test.appspot.com',
+  messagingSenderId: '374079766467',
+  appId: '1:374079766467:web:9caa7182c194dea7e24776',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
 export const influencersRef = collection(db, 'influencers'); // reference to influencers collection i firestore
+
+async function importData() {
+  const res = await fetch('./json/dininfluencer.json');
+  const data = await res.json();
+  const influencers = data;
+  console.log(data);
+
+  for (const influencer of influencers) {
+    console.log(influencer);
+
+    /* await addDoc(influencersRef, influencer); */
+  }
+}
+importData();
 
 export const usersRef = collection(db, 'users'); // reference to users collection i firestore
